2d97058f9a55365c2ca9f526c35dfc1c64afdf85
Hp <sayalichavan1106@gmail.com>
2024-10-21 11:49:14 +0530

    Initial commit

--- RobotFramework-main/config/ReportListner.py
+++
@@ -1,5 +0,0 @@
-from selenium import webdriver
-options= webdriver.ChromeOptions()
-options.add_experimental_option("detach",True)
-options = webdriver.ChromeOptions()
-driver = webdriver.Edge(service=s, options=options)
